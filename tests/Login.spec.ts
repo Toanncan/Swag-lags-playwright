cfe84e0b8c210583fda62bc6a5088238ea4e6393
Toan Can <toancan3695@gmail.com>
2025-08-05 21:47:52 +0700

    Update Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,24 +1,20 @@
-import { test, expect } from "@playwright/test"
-import { LoginPage } from "../src/pages/LoginPage"
+import { expect } from "@playwright/test";
+import { test } from "./fixtures";
 
+test.beforeEach(async ({ loginPage }) => {
+    loginPage.gotoLoginPage();
+})
 
-
-test("Don't input value", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await loginPage.gotoLoginPage();
+test("Don't input value", async ({ loginPage }) => {
     await loginPage.login("", "");
 
     await expect(await loginPage.getErrorMessage()).toBe("Epic sadface: Username is required");
-})
+});
 
 
-test("Login with invalid password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await loginPage.gotoLoginPage();
+test("Login with invalid password", async ({ loginPage }) => {
     await loginPage.login("standard_user", "111");
 
     await expect(await loginPage.getErrorMessage()).toBe("Epic sadface: Username and password do not match any user in this service");
 
-})
+});
