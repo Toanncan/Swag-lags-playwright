d6aceea6a4594beda1734f68f3de4ab595f00b2b
Toan Can <toancan3695@gmail.com>
2025-08-06 17:55:44 +0700

    update testcase Login success

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -18,3 +18,11 @@ test("Login with invalid password", async ({ loginPage }) => {
     await expect(await loginPage.getErrorMessage()).toBe("Epic sadface: Username and password do not match any user in this service");
 
 });
+
+
+test("Login with valid value", async ({ loginPage, productPage }) => {
+    await loginPage.login("standard_user", "secret_sauce");
+
+    await expect(await productPage.getProductTitlePage()).toBe("Swag Labs");
+
+});
