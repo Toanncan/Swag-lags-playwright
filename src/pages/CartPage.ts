6371645b3411b2bdff171f1d83fa31a2fa9a3615
Toan Can <toancan3695@gmail.com>
2025-08-09 13:46:54 +0700

    add  testcase for cartPage

--- src/pages/CartPage.ts
+++ src/pages/CartPage.ts
@@ -1,12 +1,30 @@
-import { Page, Locator } from "@playwright/test";
+import { Page, Locator, expect } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class CartPage extends BasePage {
     readonly productName: Locator;
+    readonly continueShoppingButton: Locator;
+    readonly checkoutButton: Locator;
+    readonly firstNameTextbox: Locator;
+    readonly lastNameTextbox: Locator;
+    readonly zipcodeTextbox: Locator;
+    readonly continueButton: Locator;
+    readonly finishButton: Locator;
+    readonly checkoutSuccessMesssage: string;
+    readonly successMessageLocator: Locator;
 
     constructor(page: Page) {
         super(page);
         this.productName = page.locator(".inventory_item_name");
+        this.continueShoppingButton = page.locator("#continue-shopping");
+        this.checkoutButton = page.locator("#checkout");
+        this.firstNameTextbox = page.locator("#first-name");
+        this.lastNameTextbox = page.locator("#last-name");
+        this.zipcodeTextbox = page.locator("#postal-code");
+        this.continueButton = page.locator("#continue");
+        this.finishButton = page.locator("#finish");
+        this.checkoutSuccessMesssage = "Your order has been dispatched, and will arrive just as fast as the pony can get there!";
+        this.successMessageLocator = page.locator(".complete-text");
     }
 
     async getOneProductNameAtUI(): Promise<string> {
@@ -23,5 +41,55 @@ export class CartPage extends BasePage {
         }
 
         return await acctualProductAtUI.sort() === products.sort();
+    }
+
+    private async getRemovedButtonByProductName(...productNames: string[]): Promise<Locator[]> {
+        const removedButtons: Locator[] = [];
+        for (const product of productNames) {
+            await removedButtons.push((await this.page.locator(`//div[text()='${product}']/parent::a/parent::div//button[text()='Remove']`)) ?? "");
+        }
+
+        return removedButtons;
+    }
+
+    async clickRemovedButtonAtProductName(...productNames: string[]) {
+        for (const removedButton of await this.getRemovedButtonByProductName(...productNames)) {
+            await removedButton.click();
+        }
+    }
+
+    async productIsRemoved(...productNames: string[]): Promise<boolean> {
+        for (const product of productNames) {
+            const isHidden = await this.page.locator(`//div[text()='${product}']`).isHidden();
+            if (!isHidden) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    async clickContinueShoppingButton() {
+        await this.continueShoppingButton.click();
+    }
+
+    async clickCheckoutButton() {
+        await this.checkoutButton.click();
+    }
+
+    async inputCheckoutInformation(firstName: string, lastName: string, zipcode: string) {
+        await this.firstNameTextbox.fill(firstName);
+        await this.lastNameTextbox.fill(lastName);
+        await this.zipcodeTextbox.fill(zipcode);
+        await this.continueButton.click();
+    }
+
+    async clickFinishButton() {
+        await this.finishButton.click();
+    }
+
+    async isProductCheckoutSuccess(): Promise<void> {
+        const acctualSuccessMessage = await this.successMessageLocator.textContent();
+
+        await expect(acctualSuccessMessage).toBe(this.checkoutSuccessMesssage);
     }
 }
--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -5,4 +5,29 @@ test.beforeEach(async ({ loginPage, productPage }) => {
     await loginPage.gotoLoginPage();
     await loginPage.login("standard_user", "secret_sauce");
 
+    await (await productPage.getAddToCartButtonByProductName("Sauce Labs Backpack")).click();
+    await productPage.clickShoppingCartIcon();
+});
+
+test("Click remove button", async ({ cartPage }) => {
+    cartPage.clickRemovedButtonAtProductName("Sauce Labs Backpack");
+
+    cartPage.productIsRemoved("Sauce Labs Backpack");
+});
+
+test("Click Continute Shopping", async ({ cartPage, productPage }) => {
+    await cartPage.clickContinueShoppingButton();
+
+    await expect(await productPage.getProductTitlePage()).toBe("Swag Labs");
+
+});
+
+test("Checkout product", async ({ cartPage }) => {
+    await cartPage.clickCheckoutButton();
+
+    await cartPage.inputCheckoutInformation("Toan", "Can", "56000");
+
+    await cartPage.clickFinishButton();
+
+    await cartPage.isProductCheckoutSuccess();
 })
